7bc87fde021d447a1d3e8e91be908fbe14a495a8
schepisii2 <schepisii2@winthrop.edu>
2021-06-27 15:53:39 -0400

    create calculator component

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
